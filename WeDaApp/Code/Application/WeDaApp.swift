373991112644947909fa2d00ad92c061c04bb79e
Jesus Chapi <jesersu@gmail.com>
2025-10-21 00:50:45 -0500

    Add app entry point
    - Add WeDaApp.swift with @main App entry point
    - Configure SwiftUI app lifecycle
    - Set MainTabView as root view

---
+++ WeDaApp/Code/Application/WeDaApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeDaApp.swift
+//  WeDaApp
+//
+//  Created by Claude Code
+//  Copyright © 2025 Dollar General. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WeDaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
